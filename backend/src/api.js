e15d8cc52da38328d75279f86675fbf9f7c0a7d6
Nelson Hernandez (NelsonCode) <50471203+nelsoncode019@users.noreply.github.com>
2021-09-11 20:03:49 -0600

    Add files via upload

---
+++ backend/src/api.js
@@ -0,0 +1,20 @@
+const games = [
+  {
+    id: 1,
+    nombre: "Fortnite",
+  },
+  {
+    id: 2,
+    nombre: "Minecraft",
+  },
+  {
+    id: 3,
+    nombre: "Call of Duty",
+  },
+  {
+    id: 4,
+    nombre: "Need for speed",
+  },
+];
+
+module.exports = games;
---
+++ frontend/src/components/Home/Home.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { onMount } from "svelte";
+  const BACKEND_URI = import.meta.env.VITE_BACKEND_URI;
+  let games = [];
+  onMount(async () => {
+    try {
+      const res = await fetch(BACKEND_URI + "/games");
+      games = await res.json();
+    } catch (error) {
+      console.log(error.response);
+    }
+  });
+</script>
+
+<h1>Hola</h1>
+<div class="box-shawdow">
+  {#each games as game}
+    <div class="grid-games">
+      <div class="box-game">
+        {game.nombre}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .box-shawdow {
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    min-height: 97vh;
+    height: 99.5%;
+    background-color: #ffffff;
+    width: 100%;
+    max-width: 600px;
+    margin: auto;
+  }
+  .grid-games {
+    display: grid;
+    grid-template-columns: 100%;
+  }
+  .box-game {
+    margin-bottom: 10px;
+    padding: 10px;
+    background: rgb(3, 3, 66);
+    color: white;
+    font-weight: 900;
+    text-align: center;
+  }
+</style>
